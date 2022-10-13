cf1db4a6cbba409f482f3f79f262aa10477a5579
Adebobola01 <m.oyedunmade@gmail.com>
2022-10-13 15:41:32 +0100

    fixed verify address bug

--- controller/auth.js
+++ controller/auth.js
@@ -7,6 +7,7 @@ let address;
 exports.getMessage = (req, res, next) => {
     address = req.body.address;
     const message = crypto.randomBytes(20).toString("hex");
+    console.log("mes");
     res.status(200).json({
         message: message,
     });
@@ -16,11 +17,9 @@ exports.verifyAddress = async (req, res, next) => {
     console.log("here");
     const message = req.body.message;
     const signature = req.body.signature;
-    const accounts = await web3.eth.personal.getAccounts();
     const verifiedAddr = await web3.eth.accounts.recover(message, signature);
     console.log(verifiedAddr.toLowerCase());
     if (address === verifiedAddr.toLowerCase()) {
-        const account = accounts[0];
         console.log("worked");
         res.status(200).json({
             message: "address is verified",
